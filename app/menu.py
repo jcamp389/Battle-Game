739f339d92977074a0300e09d9d5cfc52b9b13df
jcamp389 <32920212+jcamp389@users.noreply.github.com>
2018-01-27 14:24:51 -0600

    added game title

--- app/menu.py
+++ app/menu.py
@@ -14,8 +14,12 @@ class Menu(object):
         self.surface = surface
         self.music = music
 
+
+
         intro_screen_background = pygame.image.load('images/Intro_Screen_Background.jpg')
         self.intro_screen_background = pygame.transform.scale(intro_screen_background, (Props.SCREENLENGTH, Props.SCREENHEIGHT))
+
+        self.title = pygame.image.load("images/Hexcelsior.png")
 
         self.exit_button = Button("EXIT GAME", Props.SCREENLENGTH * .45, Props.SCREENHEIGHT * .3, 80, 40, Props.white)
         self.play_button = Button("PLAY GAME", Props.SCREENLENGTH * .45, Props.SCREENHEIGHT * .2, 80, 40, Props.white)
@@ -25,6 +29,7 @@ class Menu(object):
 
     def refresh(self):
         self.screen.blit(self.intro_screen_background, (0, 0))
+        self.screen.blit(self.title, (Props.SCREENLENGTH * .22, Props.SCREENHEIGHT * .05))
         self.exit_button.draw(self.screen)
         self.play_button.draw(self.screen)
         Utils.display_xy(self.screen)
